8165e34c9af3d5fcca7adc733c9202aac67dc06b
Gustavo <avoguga@gmail.com>
2022-06-20 22:15:31 -0300

    commitandooooo pra trocar o nome da api e tal

--- app.py
+++ app.py
@@ -8,33 +8,33 @@ from flask import Flask, request, jsonify, send_from_directory, render_template
 
 DIRETORIO = "/home/gus/Downloads/doeal/output"
 
-api = Flask(__name__)
+app = Flask(__name__)
 
-@api.route("/arquivos", methods=["GET"])
+@app.route("/arquivos", methods=["GET"])
 def lista_arquivos():
     cloudinary.config(cloud_name = os.getenv('CLOUD_NAME'), api_key=os.getenv('API_KEY'), 
       api_secret=os.getenv('API_SECRET'))
     result = resources()
     return jsonify(result)
 
-@api.route("/upload/arquivo", methods=['POST'])
+@app.route("/upload/arquivo", methods=['POST'])
 def upload_file():
-  api.logger.info('in upload route')
+  app.logger.info('in upload route')
 
   cloudinary.config(cloud_name = os.getenv('CLOUD_NAME'), api_key=os.getenv('API_KEY'), 
     api_secret=os.getenv('API_SECRET'))
   upload_result = None
   if request.method == 'POST':
     file_to_upload = request.files['file']
-    api.logger.info('%s file_to_upload', file_to_upload)
+    app.logger.info('%s file_to_upload', file_to_upload)
     if file_to_upload:
       upload_result = upload(file_to_upload)
-      api.logger.info(upload_result)
+      app.logger.info(upload_result)
       return jsonify(upload_result)
 
-@api.route('/')
+@app.route('/')
 def index():
     return "<h1>A rota para dar POST é /upload/arquivo! <br/> <br/> Para dar GET use a rota /arquivos!</h1>"
 
 if __name__ == "__main__":
-    api.run(debug=True, port=8000)
+    app.run(debug=True, port=8000)
